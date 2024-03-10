52810417f71367ba77453ac45842bf2b160cc7a3
Akshat Navlani <akshatnavlani1@gmail.com>
2024-03-10 19:45:46 +0530

    Adding the Virgil font
    Since I am unable to use a svg for the logo i am using the excalidraw font to type the logo. /n What this will do is allow us to use the font again whereever we want so we can highlight the inportant parts of the app:)

--- app/frontend/src/app/layout.tsx
+++ app/frontend/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local'
 
 const inter = Inter({ subsets: ["latin"] });
+const Virgil = localFont({ src: './Virgil.woff2' })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={Virgil.className}>{children}</body>
     </html>
   );
 }
